f00dbf882615af58bb86184041b985be3b119dbf
Gajus Kuizinas <gajus@gajus.com>
2017-04-28 13:48:38 +0100

    chore: match orinal project setup

--- src/app/store.js
+++ src/app/store.js
@@ -2,4 +2,18 @@
 
 import configureStore from './configureStore';
 
-export default configureStore();
+const store = configureStore();
+
+/**
+ * @see https://github.com/reactjs/redux/pull/667#issuecomment-255372420
+ */
+if (module.hot && module.hot.accept && typeof module.hot.accept === 'function') {
+  module.hot.accept('./rootReducer', () => {
+    // eslint-disable-next-line global-require
+    const nextRootReducer = require('./rootReducer').default;
+
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+export default store;
