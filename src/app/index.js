7c353455cea3945a9af3115ae31d0ce1d9afc699
Gajus Kuizinas <gajus@gajus.com>
2017-04-28 13:44:01 +0100

    feat: activei HMR

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,29 @@
 // @flow
 
+import {
+  AppContainer
+} from 'react-hot-loader';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+/**
+ * https://github.com/ReactTraining/react-router/issues/2704#issuecomment-261310093
+ */
+const render = () => {
+  // eslint-disable-next-line global-require
+  const App = require('./App').default;
+
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  );
+};
+
+render();
+
+if (module.hot) {
+  // $FlowFixMe
+  module.hot.accept('./App', () => {
+    render();
+  });
+}
